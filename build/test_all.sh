467732e66f68690e142e4ab98df254f432a1f799
kos59125 <mail@recyclebin.jp>
2016-01-04 19:19:57 +0900

    総時間の追加

--- build/test_all.sh
+++ build/test_all.sh
@@ -9,6 +9,7 @@ skip_count=0
 failure_count=0
 code=0
 result_collector=''
+total_elapsed=0
 
 for md in `get_markdown`
 do
@@ -18,15 +19,16 @@ do
   then
     time_test=`(time $PANDOC -o "${md%.*}.pdf" "${md}" 2>&1) 2>&1 >"$tmpfile"`
     time_test=`echo $time_test | cut -d' ' -f2 | sed -e 's/^\(.*\)m\(.*\)s$/60*\1+\2/' | bc`
+    total_elapsed=`echo "$total_elapsed + $time_test" | bc`
     error_message=`cat "$tmpfile"`
     if [ $? -eq 0 ]
     then
-      result_collector="${result_collector}<testcase classname=\"${md}\" name=\"`basename ${md}`\" time=\"$time_test\" />
+      result_collector="${result_collector}<testcase classname=\"${md}\" name=\"`basename ${md}`\" time=\"${time_test}\" />
 "
       echo "OK" >&2
     else
       failure_count=`expr $failure_count + 1`
-      result_collector="${result_collector}<testcase classname=\"${md}\" name=\"`basename ${md}`\" time=\"$time_test\">
+      result_collector="${result_collector}<testcase classname=\"${md}\" name=\"`basename ${md}`\" time=\"${time_test}\">
   <failure type=\"pandoc\"><![CDATA[${error_message}]]></failure>
 </testcase>
 "
@@ -44,8 +46,10 @@ do
 done
 
 echo "<?xml version=\"1.0\" ?>
-<testsuite name=\"pandoc\" tests=\"${test_count}\" failures=\"${failure_count}\">
-${result_collector}</testsuite>" >"${CIRCLE_TEST_REPORTS}/TestResults.xml"
+<testsuites>
+<testsuite name=\"pandoc\" tests=\"${test_count}\" failures=\"${failure_count}\" time=\"${total_elapsed}\">
+${result_collector}</testsuite>
+</testsuites>" >"${CIRCLE_TEST_REPORTS}/TestResults.xml"
 
 rm "$tmpfile"
 
