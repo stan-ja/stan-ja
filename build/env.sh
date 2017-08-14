cdb4027c4fb3c866b194c425eb6d548e6d3b3440
MatsuuraKentaro <ceiiinosssttuv@gmail.com>
2017-08-14 12:29:22 +0900

    removed --template

--- build/env.sh
+++ build/env.sh
@@ -18,6 +18,5 @@ export PANDOC_HTML="pandoc
   --standalone
   --self-contained
   --mathjax=https://gist.githubusercontent.com/yohm/0c8ed72b6f18948a2fd3/raw/624defc8ffebb0934ab459854b7b3efc563f6efb/dynoload.js
-  --css=https://gist.githubusercontent.com/griffin-stewie/9755783/raw/13cf5c04803102d90d2457a39c3a849a2d2cc04b/github.css
-  --template=build/template.html"
+  --css=https://gist.githubusercontent.com/griffin-stewie/9755783/raw/13cf5c04803102d90d2457a39c3a849a2d2cc04b/github.css"
 
